4a97625e0471667d68e95f712e9cfad84c4605c9
brunoguerra <ti@woese.com>
2013-09-24 10:54:37 -0300

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AtlantaSobralNaRecover::Application.config.secret_token = '354f15181574f9eafde3eedf2574ce57275af0ac79b9cd0b27c3e8d80b10d6e8aadf73f69d5d14f88fac5986f0721aff275529a73ec2b60100b22aacd7640f90'
